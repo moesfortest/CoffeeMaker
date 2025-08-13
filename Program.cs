92479d5c9d6f0483aeff25f379c6718281514bb5
MFTSH <MFTSH@DESKTOP-V54O0JV>
2025-08-13 19:37:05 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using CoffeeMaker;
+
+Console.WriteLine("WellCome To  CoffeeMaker");
+
+double waterLevel=0;
+double coffeeLevel=0;
+
+var vendingObjOne = new Vending(100, 120);
+
+vendingObjOne.MakeEspersso();
+ waterLevel = vendingObjOne.WaterLevel;
+ coffeeLevel = vendingObjOne.CoffeeLevel;
+Console.WriteLine(waterLevel);
+Console.WriteLine(coffeeLevel);
+Console.ReadLine();
+
+
+vendingObjOne.MakeEspersso();
+ waterLevel = vendingObjOne.WaterLevel;
+ coffeeLevel = vendingObjOne.CoffeeLevel;
+Console.WriteLine(waterLevel);
+Console.WriteLine(coffeeLevel);
+Console.ReadLine();
+
+vendingObjOne.MakeEspersso();
+waterLevel = vendingObjOne.WaterLevel;
+coffeeLevel = vendingObjOne.CoffeeLevel;
+Console.WriteLine(waterLevel);
+Console.WriteLine(coffeeLevel);
+Console.ReadLine();
+
+vendingObjOne.MakeEspersso();
+waterLevel = vendingObjOne.WaterLevel;
+coffeeLevel = vendingObjOne.CoffeeLevel;
+Console.WriteLine(waterLevel);
+Console.WriteLine(coffeeLevel);
+Console.ReadLine();
+
+vendingObjOne.MakeEspersso();
+waterLevel = vendingObjOne.WaterLevel;
+coffeeLevel = vendingObjOne.CoffeeLevel;
+Console.WriteLine(waterLevel);
+Console.WriteLine(coffeeLevel);
+Console.ReadLine();
+
+vendingObjOne.MakeEspersso();
+waterLevel = vendingObjOne.WaterLevel;
+coffeeLevel = vendingObjOne.CoffeeLevel;
+Console.WriteLine(waterLevel);
+Console.WriteLine(coffeeLevel);
+Console.ReadLine();
+
+
+
+Console.ReadLine();
+
+//var vendingObjBijar = new Vending(1000, 1000);
+
+
+//var vendingObjRodbar = new Vending(1000, 9000);
---
+++ Vending.cs
@@ -0,0 +1,99 @@
+﻿
+
+namespace CoffeeMaker
+{
+    public class Vending
+    {
+        /// <summary>
+        /// Fill Vending  Capacity
+        /// </summary>
+        public double CoffeeVolume { get; set; }
+
+        public double WaterVolume { get; set; }
+
+
+        //Display  Level Of Coffee And Water
+        public double WaterLevel { get;private set; }
+
+
+        public double CoffeeLevel { get; private set; }
+        public Vending(double coffeeInserted,double  waterInserted)
+        {
+            CoffeeVolume= coffeeInserted;
+            WaterVolume = waterInserted;
+
+
+            CoffeeLevel = coffeeInserted;
+            WaterLevel= waterInserted;
+        }
+        public  void  MakeEspersso()
+        {
+            try
+            {
+                var result = CheckResource(20, 50);
+                if (result == true)
+                {
+                    BoilWater();
+                    CoffeeGrind();
+                    BrewCoffee();
+
+                    CoffeeVolume = CoffeeVolume - 20;
+                    WaterVolume = WaterVolume - 50;
+
+
+                    CoffeeLevel = CoffeeVolume;
+                    WaterLevel = WaterVolume;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+          
+
+    
+        }
+
+        private bool CheckResource(double coffee, double water)
+        {
+
+            bool result = false;
+            if (WaterLevel == 20 || CoffeeLevel < 18 )
+            {
+                Console.WriteLine("Resource   Finshed! Check  Water Or  Coffee");
+                throw new Exception ("Coffee  Finshed!") ;
+               
+            }
+            else
+            {
+                result = true;
+            }
+
+
+
+            return result;
+             
+        }
+
+        private void BrewCoffee()
+        {
+            Console.WriteLine("BrewCoffee");
+        }
+
+        private void CoffeeGrind()
+        {
+            Console.WriteLine("CoffeeGrind");
+        }
+
+        private void BoilWater()
+        {
+            Console.WriteLine("BoilWater");
+        }
+
+        //private void   Payment()
+        //{
+
+        //}
+    }
+}
